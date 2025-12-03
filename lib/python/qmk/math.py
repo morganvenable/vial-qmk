2e371cece71e68fe41af812419bf75df9ae390f0
morganvenable <venable@gmail.com>
2025-12-03 10:20:30 -0800

    python math fix?

--- lib/python/qmk/math.py
+++ lib/python/qmk/math.py
@@ -23,7 +23,7 @@ def compute(expr):
 
 
 def _eval(node):
-    if isinstance(node, ast.Num):  # <number>
+    if isinstance(node, ast.Constant) and isinstance(node.value, (int, float)):  # <number>
         return node.n
     elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
         return operators[type(node.op)](_eval(node.left), _eval(node.right))
